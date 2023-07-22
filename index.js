0a18f42ba312b69a0bb3baf7796c196f9a82be8c
x.ande <ananfro@live.com>
2023-07-22 10:32:57 +1000

    first take at readme template

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ inquirer
     ])
 
     .then();
+
+
+    //Project name
+    //readme: sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+//prompt: description, installation instructions, usage information, contribution guidelines, and test instructions
+//prompt: choose a license for my application from a list of options
+//a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+//prompt: github username => questions with a link to gh profile
+//prompt: email => questions
+//with instructions how to reach me with additional questions
